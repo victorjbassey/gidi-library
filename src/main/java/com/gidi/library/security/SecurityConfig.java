2a3731c2c28d83879819911d42cf777dbe661924
Victor Bassey <viktorbasi545@gmail.com>
2020-03-19 19:22:57 +0100

    Set up spring security

---
+++ src/main/java/com/gidi/library/security/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.gidi.library.security;
+
+import com.gidi.library.service.LibraryUserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**",
+
+            // registration and login
+            "/register",
+            "/login",
+
+            // H2 console
+            "/h2/**"
+    };
+
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+    private LibraryUserDetailService libraryUserDetailService;
+    private JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(JwtAuthenticationEntryPoint unauthorizedHandler,
+                          LibraryUserDetailService libraryUserDetailService, JwtRequestFilter jwtRequestFilter) {
+        this.unauthorizedHandler = unauthorizedHandler;
+        this.libraryUserDetailService = libraryUserDetailService;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(libraryUserDetailService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/books/*").hasRole("LIBRARIAN")
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        // This line below is necessary to allow H2 in-memory database work with spring security.
+        // I wouldn't do this in production since I'd be working with a real database
+        http.headers().frameOptions().sameOrigin();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
