ff25469856c5607fcbee3ae533c2549446d19fe0
Victor Bassey <viktorbasi545@gmail.com>
2020-03-20 16:06:50 +0100

    Add static files for UI

---
+++ src/main/resources/static/precache-manifest.726ea746fbf01d34eb66d9bc72f7d577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5803588ba127d8054ebd02130cd4d641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d5c6803ccaf953c87e4",
+    "url": "/static/css/main.3592fdeb.chunk.css"
+  },
+  {
+    "revision": "82753872b2c7e6d395f0",
+    "url": "/static/js/2.4a442d25.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/static/js/2.4a442d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5c6803ccaf953c87e4",
+    "url": "/static/js/main.198c0207.chunk.js"
+  },
+  {
+    "revision": "8686d34071af68d78f69",
+    "url": "/static/js/runtime-main.18b821e1.js"
+  }
+]);
